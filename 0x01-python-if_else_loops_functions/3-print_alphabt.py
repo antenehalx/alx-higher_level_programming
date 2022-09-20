fb242117acdf60bb810838732870d4732f5cf279
antenehalx <antenehalx@github.com>
2022-09-20 20:03:39 +0000

    python

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+ #!/usr/bin/python3
+ in range(ord('a'), ord('z') + 1):
+    if chr(a) != 'e' and chr(a) != 'q':
+        print("{:c}".format(a), end='')
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+ in range(0, 99):
+    print("{:d} = 0x{:x}".format(i, i))
